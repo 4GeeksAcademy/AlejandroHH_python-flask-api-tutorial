6cd79c8e8f6c0924d576c1961347bd01b74fff20
Alejandro Hipólito Hernando <126016967+Alejandro-Hipolito@users.noreply.github.com>
2023-05-31 20:33:15 +0000

    Success

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+todos = [
+  {"label": "My first task", "done": False},
+]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+  json_tasks = jsonify(todos)
+  return json_tasks
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    del todos[position]
+    return jsonify(todos)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
